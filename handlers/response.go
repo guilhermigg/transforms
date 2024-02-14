ade757ff9c37cae5a20194ccd30ab0d860274e26
guilhermigg <guilhermigg.e@gmail.com>
2024-02-14 19:54:12 -0300

    sendSuccess response

--- handlers/response.go
+++ handlers/response.go
@@ -1,11 +1,23 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendError(ctx *gin.Context, msg string, code int) {
 	ctx.Header("Content-type", "application/json")
 	ctx.JSON(code, gin.H{
 		"message": msg,
 		"code":    code,
+	})
+}
+
+func sendSuccess(ctx *gin.Context, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": http.StatusOK,
+		"data": data,
 	})
 }
