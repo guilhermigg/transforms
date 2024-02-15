2b9c4943b6a53455e7ab1f3e9e0ad9e5e2786b8e
guilhermigg <guilhermigg.e@gmail.com>
2024-02-14 23:44:19 -0300

    delete user function

--- handlers/deleteUser.go
+++ handlers/deleteUser.go
@@ -1,13 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"transforms/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DeleteUserHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE user",
+	var user schemas.User
+	userIdParam := ctx.Param("id")
+
+	userId, err := strconv.ParseUint(userIdParam, 10, 64)
+	if err != nil {
+		sendError(ctx, "invalid user id", http.StatusBadRequest)
+		return
+	}
+
+	user.ID = uint(userId)
+	fmt.Println(user)
+
+	if err := db.Delete(&user).Error; err != nil {
+		sendError(ctx, "could not delete this user", http.StatusBadRequest)
+		return
+	}
+
+	sendSuccess(ctx, gin.H{
+		"msg": "user successfully deleted",
 	})
 }
